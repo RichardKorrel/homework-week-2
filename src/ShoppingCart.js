190761b85156f9ed8b54bdd61585ca8f7f3e0e5c
RichardKorrel <richard.korrel1@gmail.com>
2018-06-10 14:35:28 +0200

    added methods getItems and addItem to the ShoppingCart class

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,5 +1,24 @@
 // Declare a class ShoppingCart
 class ShoppingCart {
+  // The ShoppingCart has items, initially empty
+  constructor (items) {
+    this.items = []
+  }
+  // Get the items that are in the ShoppingCart
+  getItems() {
+    return this.items
+  }
+  // Add a new item in the ShoppingCart
+  addItem(itemName, quantity, price) {
+    // an item consists of a name, quantity and price
+    const item = {
+      name: itemName,
+      quantity: quantity,
+      pricePerUnit:price
+    }
+    // Concatenate the item to the items list
+    this.items=this.items.concat(item)
+  }
 }
 
 // Export the class ShoppingCart

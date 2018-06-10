65013887c6556f03c3ae6bdc17f21826fdc3880a
RichardKorrel <richard.korrel1@gmail.com>
2018-06-10 14:57:43 +0200

    updated function giveItBackLater with parameters and logic

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,5 +1,15 @@
-// Declare function giveItBackLater
+// Declare function giveItBackLater and pass it
+// a parameter called value and a parameter called
+// callback which is a function
 function giveItBackLater(value, callback){
+  // Declare function willBeCalledLater that will call
+  // the callback function
+  function willBeCalledLater() {
+      callback(value)
+  }
+  // Call the function willBeCalledLater that calls the
+  // callback function but only after 100 milliseconds
+  setTimeout(willBeCalledLater, 100)
 }
 
 // Declare function promiseToGiveItBackLater

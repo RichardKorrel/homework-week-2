94f6672efa7ba9232085025c958101c0d5febc7f
RichardKorrel <richard.korrel1@gmail.com>
2018-06-10 14:55:34 +0200

    created module and declared the async functions and exported them

---
+++ src/async-functions.js
@@ -0,0 +1,18 @@
+// Declare function giveItBackLater
+function giveItBackLater(value, callback){
+}
+
+// Declare function promiseToGiveItBackLater
+function promiseToGiveItBackLater(value){
+}
+
+// Declare function addSomePromises
+function addSomePromises(somePromise){
+}
+
+// Export the fuctions
+module.exports = {
+  giveItBackLater,
+  addSomePromises,
+  promiseToGiveItBackLater
+}
